e6424b1b8cd8ccbcdc1bc800e2f977260a81d05c
eddiechapman <eddie.chapman4@gmail.com>
2019-10-30 08:47:20 -0500

    Add degree dictionary upon new degree document.

--- merge_docs.py
+++ merge_docs.py
@@ -66,7 +66,9 @@ def main(args):
         m = re.search(pattern, f.name, re.IGNORECASE)
         if m:
             logging.debug(f'{f} matched: {m.groupdict()}')
-            documents[str(m.group('id'))][m.group('category').lower()] = f
+            if m.group('id') not in documents:
+                documents[(m.group('id')] = dict()
+            documents[(m.group('id')][m.group('category').lower()] = f
 
     # Process sorted documents
     for degree, docs in documents:
